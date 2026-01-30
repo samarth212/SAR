7ffd93b92d15f2c50b280230adffa9cc58623379
Samarth Kolanupaka <samarth212@gmail.com>
2026-01-29 23:54:16 -0500

    restarting -- establishing web socket with boost

---
+++ backend/socket.cpp
@@ -0,0 +1,11 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/error.hpp>
+#include <boost/asio/ssl/stream.hpp>
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
