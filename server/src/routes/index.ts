61bcb4b36c3c556f51b770ac312e4e8a14718a21
Samarth Kolanupaka <samarth212@gmail.com>
2026-02-23 12:44:57 -0500

    wire index.ts to c++ api

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,8 +2,12 @@ import { Router } from 'express';
 
 export const router = Router();
 
+const url = `${process.env.CPP_API_BASE_URL}/api/anomalies`;
+
 router.get('/get-anomaly', async (req, res, next) => {
   try {
+    const data = await fetch(url, { method: 'GET' });
+    console.log(data);
   } catch (err) {
     next(err);
   }
